039cc4ba853729912254912e5c1e19ab519c8058
r0lm0 <rolmo92@gmail.com>
2025-09-05 06:53:19 -0600

    first commit

---
+++ src/main.ts
@@ -0,0 +1,23 @@
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+import { ValidationPipe, Logger } from '@nestjs/common';
+import { envs } from './config';
+
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
+  const logger = new Logger('Bootstrap');
+
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      forbidNonWhitelisted: true,
+    })
+  );
+
+  await app.listen(envs.port);
+  logger.log(`Orders Microservice running on port ${envs.port}`);
+}
+bootstrap();
+
+
