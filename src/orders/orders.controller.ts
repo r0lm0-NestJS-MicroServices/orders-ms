29b507861e4962c7d511806c7590aca56324c0cf
r0lm0 <rolmo92@gmail.com>
2025-09-05 15:35:14 -0600

    Refactor OrdersController and OrdersService to improve command patterns and remove unused methods. Updated message patterns to use a consistent naming convention and added ParseIntPipe for ID parsing in findOne method.

--- src/orders/orders.controller.ts
+++ src/orders/orders.controller.ts
@@ -1,29 +1,29 @@
-import { Controller, NotImplementedException } from '@nestjs/common';
+import { Controller, NotImplementedException, ParseIntPipe } from '@nestjs/common';
 import { MessagePattern, Payload } from '@nestjs/microservices';
 import { OrdersService } from './orders.service';
 import { CreateOrderDto } from './dto/create-order.dto';
-import { UpdateOrderDto } from './dto/update-order.dto';
+
 
 @Controller()
 export class OrdersController {
   constructor(private readonly ordersService: OrdersService) { }
 
-  @MessagePattern('createOrder')
+  @MessagePattern({ cmd: 'create_order' })
   create(@Payload() createOrderDto: CreateOrderDto) {
     return this.ordersService.create(createOrderDto);
   }
 
-  @MessagePattern('findAllOrders')
+  @MessagePattern({ cmd: 'find_all_orders' })
   findAll() {
     return this.ordersService.findAll();
   }
 
-  @MessagePattern('findOneOrder')
-  findOne(@Payload() id: number) {
-    return this.ordersService.findOne(id);
+  @MessagePattern({ cmd: 'find_order_by_id' })
+  findOne(@Payload('id', ParseIntPipe) id: number) {
+    return this.ordersService.findOne(+id);
   }
 
-  @MessagePattern('changeOrderStatus')
+  @MessagePattern({ cmd: 'change_order_status' })
   changeOrderStatus() {
     throw new NotImplementedException('Not implemented');
   }
--- src/orders/orders.service.ts
+++ src/orders/orders.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@nestjs/common';
 import { CreateOrderDto } from './dto/create-order.dto';
-import { UpdateOrderDto } from './dto/update-order.dto';
+
 
 @Injectable()
 export class OrdersService {
@@ -16,11 +16,5 @@ export class OrdersService {
     return `This action returns a #${id} order`;
   }
 
-  update(id: number, updateOrderDto: UpdateOrderDto) {
-    return `This action updates a #${id} order`;
-  }
 
-  remove(id: number) {
-    return `This action removes a #${id} order`;
-  }
 }
